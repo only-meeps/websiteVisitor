badb33a652eae1a06e00ec5d9b8d4450e4edfc2b
AwesomeMeeps <151784194+only-meeps@users.noreply.github.com>
2025-12-04 20:45:56 -0800

    updated index.html and main.js

--- Main.js
+++ Main.js
@@ -10,7 +10,18 @@ function goToNextPage()
 function readInput()
 {
     const inputElement = document.getElementById("userInput")
-    const inputValue = inputElement.inputValue;
-    document.getElementById("abutton").textContent = 
-    "You entered: " + inputValue;
+    const inputValue = inputElement.value;
+    document.getElementById("abutton").textContent = "You entered: " + inputValue;
+}
+function downloadTxtFile() {
+    const textContent = "Heeeyyyy you downloaded this file!";
+    const filename = 'generated_file.txt';
+    const blob = new Blob([textContent], { type: 'text/plain' });
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = filename;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    URL.revokeObjectURL(downloadLink.href);
 }
