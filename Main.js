b22ed118bc8a8d31dc7190179a7aeca0458dba0b
AwesomeMeeps <ffkoenig@gmail.com>
2025-12-09 17:20:04 -0800

    Added playfab login

--- Main.js
+++ Main.js
@@ -31,3 +31,27 @@ async function downloadTxtFile() {
         alert('Failed to load or process the template file.');
     }
 }
+function PlayFabSignIn(){
+    PlayFab.settings.titleId = "1F918E";
+    var loginRequest = {
+        // Currently, you need to look up the required and optional keys for this object in the API reference for LoginWithCustomID. See the Request Headers and Request Body.
+        TitleId: PlayFab.settings.titleId,
+        CustomId: crypto.randomUUID(),
+        CreateAccount: true
+    };
+
+    PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
+}
+
+// callback functions take two parameters: result and error
+// see callback functions in JavaScript if unclear
+var LoginCallback = function (result, error) {
+    if (result !== null) {
+        document.getElementById("resultOutput").innerHTML = "Congratulations, you made your first successful API call!";
+    } else if (error !== null) {
+        document.getElementById("resultOutput").innerHTML =
+            "Something went wrong with your first API call.\n" +
+            "Here's some debug information:\n" +
+            PlayFab.GenerateErrorReport(error);
+    }
+}
